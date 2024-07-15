b724e010adff3ebec99f44c0423724f0f4782916
saksham <sakshamattri3@gmail.com>
2024-07-15 22:10:19 +0530

    initialize project

---
+++ src/main/java/suck/some/lilOJ/LilOjApplication.java
@@ -0,0 +1,13 @@
+package suck.some.lilOJ;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LilOjApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LilOjApplication.class, args);
+	}
+
+}
---
+++ src/test/java/suck/some/lilOJ/LilOjApplicationTests.java
@@ -0,0 +1,13 @@
+package suck.some.lilOJ;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LilOjApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
