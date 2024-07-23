fba282343156d6b946ca1f11f7e15cb90786c89f
saksham <sakshamattri3@gmail.com>
2024-07-23 23:14:43 +0530

    basic jdbc auth with postgres docker image

--- src/main/java/suck/some/lilOJ/config/SecurityConfig.java
+++ src/main/java/suck/some/lilOJ/config/SecurityConfig.java
@@ -1,13 +1,19 @@
 package suck.some.lilOJ.config;
 
 import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import javax.sql.DataSource;
+
+// import org.springframework.boot.autoconfigure.batch.BatchProperties.Jdbc;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+// import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -16,9 +22,12 @@ import org.springframework.security.web.SecurityFilterChain;
 public class SecurityConfig {
 
     @Bean
-    InMemoryUserDetailsManager userDetailsManager() {
-        return new InMemoryUserDetailsManager(
-                User.withUsername("lol").password("{noop}password").roles("ADMIN").build());
+    JdbcUserDetailsManager userDetailsManager(DataSource dataSource) {
+        JdbcUserDetailsManager jdbcUserDetailsManager = new JdbcUserDetailsManager(dataSource);
+        UserDetails admin = User.builder().username("admin").password("{noop}password").roles("ADMIN").build();
+        if (!jdbcUserDetailsManager.userExists(admin.getUsername()))
+            jdbcUserDetailsManager.createUser(admin);
+        return jdbcUserDetailsManager;
     }
 
     @Bean
